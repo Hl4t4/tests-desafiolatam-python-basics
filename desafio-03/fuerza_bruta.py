dfb85ab20bdafb4e04ef6e47f712b4fc39f0dbf0
Jose Latapiatt <jose.latapiatt@proton.me>
2024-03-01 23:01:58 -0300

    Desafio evaluado 03 actividad 3

---
+++ desafio-03/fuerza_bruta.py
@@ -0,0 +1,65 @@
+from string import ascii_lowercase
+import getpass #Para passwords
+import re # Para expresiones regulares
+import timeit #Testing de tiempos, no necesario realmente
+
+pattern = re.compile("[a-z]+")
+password = getpass.getpass("Ingrese la contraseña: ")
+match = pattern.match(password)
+
+while (not match):
+    password = getpass.getpass("Entrada no valida solo se aceptan letras mayusculas y minusculas. ").lower()
+    match = pattern.match(password)
+
+
+#Contar la posicion de la letra para saber cantidad de intentos 
+difficulty = 0
+for char in password:
+    difficulty+= ord(char) - ord('a') + 1
+
+print(f"La contraseña fue forzada en {difficulty} intentos")
+
+# Usando for
+difficulty = 0
+for char in password:
+    for ascii_char in ascii_lowercase:
+        difficulty+= 1
+        if char == ascii_char:
+            break
+print(f"La contraseña fue forzada en {difficulty} intentos")
+
+# Usando comprehensions
+
+difficulty = sum([sum([1 for ascii_char in ascii_lowercase if ascii_char <= char]) for char in password])
+print(f"La contraseña fue forzada en {difficulty} intentos")
+
+
+#############
+
+#TESTING TIMES
+
+#############
+
+print("\n\nTesting times\n\n")
+
+def test01():
+    difficulty = 0
+    for char in password:
+        difficulty+= ord(char) - ord('a') + 1
+    #print(f"La contraseña fue forzada en {difficulty} intentos")
+print(f"Metodo que solo cuenta por la letra {timeit.timeit('test01()',setup='from __main__ import test01' , number=10000)}")
+
+def test02():
+    difficulty = 0
+    for char in password:
+        for ascii_char in ascii_lowercase:
+            difficulty+= 1
+            if char == ascii_char:
+                break
+    #print(f"La contraseña fue forzada en {difficulty} intentos")
+print(f"Metodo usando for {timeit.timeit('test02()',setup='from __main__ import test02' , number=10000)}")
+
+def test03():
+    difficulty = sum([sum([1 for ascii_char in ascii_lowercase if ascii_char <= char]) for char in password])
+    #print(f"La contraseña fue forzada en {difficulty} intentos")
+print(f"Metodo usando comprehensions {timeit.timeit('test03()',setup='from __main__ import test03' , number=10000)}")
