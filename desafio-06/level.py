108c4c1334e8cf5e47cd8f59ddefe12ae2ffc62c
Jose Latapiatt <jose.latapiatt@proton.me>
2024-03-06 19:57:46 -0300

    validador y level

---
+++ desafio-06/level.py
@@ -0,0 +1,25 @@
+def choose_level(n_pregunta, p_level):
+    
+    # Construir lógica para escoger el nivel
+    ##################################################
+    if p_level <= 3 and n_pregunta <= p_level*3: # se puede liberar la segunda condicion para que cualquier numero alto sea elegir nivel avanzado
+        n_level = n_pregunta/p_level
+        if n_level <= 1:
+            level = "basicas"
+        elif n_level <= 2:
+            level = "intermedias"
+        else:
+            level = "avanzadas"
+    else:
+        level = "avanzadas"
+    
+    ##################################################
+    
+    return level
+
+if __name__ == '__main__':
+    # verificar resultados
+    print(choose_level(2, 2)) # básicas
+    print(choose_level(3, 2)) # intermedias
+    print(choose_level(7, 2)) # avanzadas
+    print(choose_level(4, 3)) # intermedias
---
+++ desafio-06/preguntas.py
@@ -0,0 +1,13 @@
+preguntas_basicas = {"pregunta_1": {"enunciado":"enunciado 01", "alternativas": [["alt_0",0], ["alt_1",0], ["alt_2",1], ["alt_3",0]]},
+                     "pregunta_2": {"enunciado":"enunciado 02", "alternativas": [["alt_0",0], ["alt_1",0], ["alt_2",1], ["alt_3",0]]},
+                     "pregunta_3": {"enunciado":"enunciado 03", "alternativas": [["alt_0",0], ["alt_1",0], ["alt_2",1], ["alt_3",0]]}}
+preguntas_intermedias = {"pregunta_1": {"enunciado":"enunciado 01", "alternativas": [["alt_0",0], ["alt_1",0], ["alt_2",1], ["alt_3",0]]},
+                     "pregunta_2": {"enunciado":"enunciado 02", "alternativas": [["alt_0",0], ["alt_1",0], ["alt_2",1], ["alt_3",0]]},
+                     "pregunta_3": {"enunciado":"enunciado 03", "alternativas": [["alt_0",0], ["alt_1",0], ["alt_2",1], ["alt_3",0]]}}
+preguntas_avanzadas = {"pregunta_1": {"enunciado":"enunciado 01", "alternativas": [["alt_0",0], ["alt_1",0], ["alt_2",1], ["alt_3",0]]},
+                     "pregunta_2": {"enunciado":"enunciado 02", "alternativas": [["alt_0",0], ["alt_1",0], ["alt_2",1], ["alt_3",0]]},
+                     "pregunta_3": {"enunciado":"enunciado 03", "alternativas": [["alt_0",0], ["alt_1",0], ["alt_2",1], ["alt_3",0]]}}
+
+pool_preguntas = {"basicas": preguntas_basicas,
+                  "intermedias": preguntas_intermedias,
+                  "avanzadas": preguntas_avanzadas}
